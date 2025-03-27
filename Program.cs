49efe790bd27d38c6816efffebb05e42d8f52350
md7426 <maurodavimedeiros@gmail.com>
2025-03-27 18:02:53 -0300

    implementado a abstração de celulares nokia e iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone da Nokia:");
+Nokia nokia = new Nokia(numero: "1234567899", modelo: "modelo 1", imei: "837463567273876", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Github");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone(numero: "1234567899", modelo: "modelo 1", imei: "837463567273876", memoria: 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
